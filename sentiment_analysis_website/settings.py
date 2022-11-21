ed76faa38d89ce9635085507ae370c97934d13b1
suyog9139 <suyogwalunj3@gmail.com>
2022-11-21 19:30:34 +0530

    003_state

--- sentiment_analysis_website/settings.py
+++ sentiment_analysis_website/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sentiment_analysis_website.urls'
@@ -133,4 +132,3 @@ STATICFILES_DIRS ={
 
 MODELS = os.path.join(BASE_DIR, 'sentiment/models')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
